385b80c245f75497e567777559bb17f70d314a20
Joe Prathap P J <joeprathappj@gmail.com>
2024-01-11 10:37:27 +0530

    refactor: debug

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ io.of("/api/socket").on("connection",(socket)=>{
   console.log("socket.io: User connected: ",socket.id);
   console.log(socket.handshake)
   let user = socket.handshake.query?.user
-  users[socket.id] = user && user.name ? user.name : socket.id;
+  users[socket.id] = user && user ? user: socket.id;
   io.of("/api/socket").emit("online",users)
 
 
